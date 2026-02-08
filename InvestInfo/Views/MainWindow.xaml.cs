9c74e335413822bb3685e80281eb2c72a29a6f3b
kleom <kleom@STARTEL>
2026-02-08 16:32:32 +0300

    Добавьте файлы проекта.

---
+++ InvestInfo/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvestInfo.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Закрыть окно
+        private void CloseBtn_Click(object sender, RoutedEventArgs e) =>
+            Close();
+
+        // Увеличить, уменьшить окно
+        private void MaximizeBtn_Click(object sender, RoutedEventArgs e) =>
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        // Сворачивание окна
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e) =>
+            WindowState = WindowState.Minimized;
+
+        // Перемещение окна
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+    }
+}
---
+++ InvestInfo/Views/OperationsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvestInfo.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для OperationsView.xaml
+    /// </summary>
+    public partial class OperationsView : Page
+    {
+        public OperationsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
